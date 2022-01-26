22480c852e68cc235be8e20f9ad1097b62ed7c1e
Cody Lerum <cody.lerum@gmail.com>
2022-01-26 11:19:23 -0700

    add logging for exception

--- src/main/java/demo/EventObserver.java
+++ src/main/java/demo/EventObserver.java
@@ -12,14 +12,24 @@ public class EventObserver {
   private static final Logger log = Logger.getLogger("EventObserver");
 
   public void observe(@Observes SimpleEvent simpleEvent) throws InterruptedException {
-    log.info(String.format("Observing Synchronously %s and sleeping for %s ms", simpleEvent.getId(), simpleEvent.getSleepDuration().toMillis()));
-    Thread.sleep(simpleEvent.getSleepDuration().toMillis());
-    log.info(String.format("Finished %s", simpleEvent.getId()));
+    try {
+      log.info(String.format("Observing Synchronously %s and sleeping for %s ms", simpleEvent.getId(), simpleEvent.getSleepDuration().toMillis()));
+      Thread.sleep(simpleEvent.getSleepDuration().toMillis());
+      log.info(String.format("Finished %s", simpleEvent.getId()));
+    } catch (InterruptedException e) {
+      log.info("InterruptedException was thrown");
+      throw e;
+    }
   }
 
   public void observeAsync(@ObservesAsync SimpleEvent simpleEvent) throws InterruptedException {
-    log.info(String.format("Observing Asynchronously  %s and sleeping for %s ms", simpleEvent.getId(), simpleEvent.getSleepDuration().toMillis()));
-    Thread.sleep(simpleEvent.getSleepDuration().toMillis());
-    log.info(String.format("Finished %s", simpleEvent.getId()));
+    try {
+      log.info(String.format("Observing Asynchronously  %s and sleeping for %s ms", simpleEvent.getId(), simpleEvent.getSleepDuration().toMillis()));
+      Thread.sleep(simpleEvent.getSleepDuration().toMillis());
+      log.info(String.format("Finished %s", simpleEvent.getId()));
+    } catch (InterruptedException e) {
+      log.info("InterruptedException was thrown");
+      throw e;
+    }
   }
 }
